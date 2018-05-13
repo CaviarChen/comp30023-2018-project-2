c2bd4d11771146686aeb9444b67c22aa6f79485d
CaviarChen <yzj@vlabpro.com>
2018-05-13 16:50:36 +1000

    complete: getting CN&SAN

--- src/cert_helper.c
+++ src/cert_helper.c
@@ -22,7 +22,9 @@
 
 int cert_verify_dates(X509 *cert, BIO *outbio);
 int cert_verify_domain(X509 *cert, BIO *outbio, const char* domain);
+
 int check_domain(const char* domain, const char* target_domain);
+char* astr_to_str(ASN1_STRING* a_str);
 
 void cert_init() {
     //initialise openSSL
@@ -132,27 +134,43 @@ int cert_verify_domain(X509 *cert, BIO *outbio, const char* domain) {
     if(check_domain(domain, cert_cn)) return TRUE;
 
     // SAN
+    GENERAL_NAMES *g_names;
+    g_names = X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
 
+    if (g_names!=NULL) {
+        int count = sk_GENERAL_NAME_num(g_names);
+        for(int i=0; i<count; i++) {
+            GENERAL_NAME *g_name = sk_GENERAL_NAME_value(g_names, i);
+            if(g_name->type != GEN_DNS) continue;
 
+            ASN1_IA5STRING * a_str = g_name->d.dNSName;
+            char* buf = astr_to_str(a_str);
 
-    // int loc = -1;
-    // while ((loc = X509_get_ext_by_NID(cert, NID_subject_alt_name, loc))!=-1) {
-    //     X509_EXTENSION *ex = X509_get_ext(cert, loc);
-    //     ASN1_OCTET_STRING *ao_str = X509_EXTENSION_get_data(ex);
-    //
-    //     char *buf = (char *)malloc((ao_str->length + 1) * sizeof(char));
-    //     memcpy(buf, ao_str->data, ao_str->length);
-    //     buf[ao_str->length] = '\0';
-    //
-    //     printf("AAAA: %s", buf);
-    //
-    //     free(buf);
-    //
-    // }
+            #if DEBUG
+            printf("SAN: %s\n", buf);
+            #endif
 
+            if(check_domain(domain, buf)) {
+                free(buf);
+                return TRUE;
+            }
 
+            free(buf);
+
+        }
+
+    }
 
     return FALSE;
+}
+
+char* astr_to_str(ASN1_STRING* a_str) {
+    char *buf = (char *)malloc((a_str->length + 1) * sizeof(char));
+
+    memcpy(buf, a_str->data, a_str->length);
+    buf[a_str->length] = '\0';
+
+    return buf;
 }
 
 int check_domain(const char* domain, const char* target_domain) {
